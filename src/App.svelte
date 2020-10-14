5f8c1ac4fd5a3263a26a33b159bb36394fe74445
Nicolò Valenza <nico.vale.spazio@outlook.com>
2020-10-14 15:58:53 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import Home from "./routes/Home.svelte";
+	import About from "./routes/About.svelte";
+	import Project from "./routes/Article.svelte";
+	import Article from "./routes/Project.svelte";
+   
+	export let url = "";
+  </script> 
+   
+  <Router url="{url}">
+	<nav>
+	  <Link to="/">Home</Link>
+	  <Link to="about">About</Link>
+	  <Link to="article">Article</Link>
+	  <Link to="project">Project</Link>
+	</nav>
+	<div>
+	  <Route path="about" component="{About}" />
+	  <Route path="/"><Home /></Route>
+	  <Route path="article" component="{Article}" />
+	  <Route path="project" component="{Project}" />
+	</div>
+  </Router>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,11 @@
+<p id="title">Algorithm-net</p>
+<style>
+    p {
+        color: black;
+
+        text-shadow: 0 0 10px #cebed1, 0 0 150px black ;
+         
+
+        font-family: Consolas,monaco,monospace;
+    }
+</style>
